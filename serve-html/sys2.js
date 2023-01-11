d7e3975fc2d390dac270584b90ed0d9c0dc3041f
Nknight AMAMIYA <88763245+NknightA@users.noreply.github.com>
2023-01-11 20:42:47 +0900

    (XD)

---
+++ serve-html/sys2.js
@@ -0,0 +1,2 @@
+var link = "https://twitter.com/ama_p213";
+console.log('follow me! -> @ama_p213' + ' ' + link);
---
+++ src/sys2.ts
@@ -0,0 +1,3 @@
+const link: string = "https://twitter.com/ama_p213";
+
+console.log('follow me! -> @ama_p213'+ ' ' + link )
